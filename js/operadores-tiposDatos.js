b47e760a27bc26d6fc2c145fab2210f62d02c5c9
Gustavo Solano <gus20solji@gmail.com>
2018-09-08 23:58:26 -0600

    Operadores y tipos de datos, funciones básicas

---
+++ js/operadores-tiposDatos.js
@@ -0,0 +1,28 @@
+'use strict'
+
+//operadores
+
+var numero1 = 7;
+
+var numero2 = 12;
+
+var operacion =  numero1*numero2;
+
+alert("El resultado de la operación es: "+operacion);
+
+
+//tipos de datos
+
+var numero_entero  = 50; //int
+var cadena_texto = "Gustavo Solano"; //string
+var verdadero_o_falso = true; //booleano
+
+var numero_falso = "33";
+//funcion number() convierte los elementos en numero
+//Number();
+
+//funcion String() convierte los numeros en string
+//String()
+//parseInt -> parsea un float a int
+console.log(verdadero_o_falso);
+
