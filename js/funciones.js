57a7cc63a10cfa3fea4421901d355bba3d7cb59b
Unknown <gus20solji@gmail.com>
2018-10-29 21:54:11 -0600

    Ejercicio 8, inicio de funciones

---
+++ js/funciones.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//FUNCIONES
+// una funcion es una agrupacion reutilizable de un conjunto de instrucciones
+
+function calculadora(){
+
+    return "Hola soy la calculadora";
+}
+
+console.log(calculadora());
